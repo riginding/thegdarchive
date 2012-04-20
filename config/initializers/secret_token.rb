35d8445792c9b6d69f130771d93213ed82b0396d
Ryan Riginding <marc.riginding@gmail.com>
2012-04-20 13:22:13 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thegdarchive::Application.config.secret_token = 'f4f3739e724e9cd4b189b5c5f4526d78625d480926d5c789fb6a762fde036050392733b4ebff8da87529dc71fee341e73bbb70e707f690ae4712c3d1aed2ed93'
