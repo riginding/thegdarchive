58fdd157e50f6cd62f4343224ac8d76a27202a47
Ryan Riginding <marc.riginding@gmail.com>
2012-05-26 14:48:15 +0200

    changed timestamp/timeat input

--- app/controllers/timestamps_controller.rb
+++ app/controllers/timestamps_controller.rb
@@ -1,12 +1,27 @@
 class TimestampsController < ApplicationController
   def create
     @vod = Vod.find(params[:vod_id])
-    @timestamp = Timestamp.new(params[:timestamp])
+    content = params[:timestamp][:content]
+    time = convert_time(params[:timestamp][:timeat])
+    @timestamp = Timestamp.new(content: content, timeat: time)
     @timestamp.vod = @vod
     @timestamp.save!
     respond_to do |format|
       format.html { redirect_to @timestamp.vod }
       format.js
+    end
+  end
+
+  private
+  def convert_time(time)
+    if time.split(':').size == 3
+      hours, minutes, seconds = time.split(':')
+      return Integer(hours) * 60 * 60 + Integer(minutes) * 60 + Integer(seconds)
+    elsif time.split(':').size == 2
+      minutes, seconds = time.split(':')
+      return Integer(minutes) * 60 + Integer(seconds)
+    else
+      return Integer(time)
     end
   end
 end
