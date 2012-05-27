4d985e588f1fc8d3459edb95b999a003c7764bc0
Ryan Riginding <marc.riginding@gmail.com>
2012-05-27 22:03:02 +0200

    changed timestamp model

--- app/models/timestamp.rb
+++ app/models/timestamp.rb
@@ -1,6 +1,6 @@
 class Timestamp < ActiveRecord::Base
   belongs_to :vod
 
-  validates :timeat, presence: true
-  validates :content, presence: true
+  validates :timeat, presence: true, uniqueness: true
+  validates :content, presence: true, uniqueness: true
 end
