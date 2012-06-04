8e36efb71ce070cbe743f14bb11e128ccd3f5164
Ryan Riginding <marc.riginding@gmail.com>
2012-06-04 16:03:36 +0200

    only save unique impressions

--- app/models/vod.rb
+++ app/models/vod.rb
@@ -3,7 +3,7 @@ class Vod < ActiveRecord::Base
   belongs_to :game
   belongs_to :show
 
-  is_impressionable counter_cache: {column_name: :views}
+  is_impressionable counter_cache: {column_name: :views, unique: true}
 
   default_scope where(accessible: true)
   scope :most_watched, lambda { where("views is not null").order("views DESC").first(3) }
