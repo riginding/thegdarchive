8a6d6986f41b52d9254bee76f4791db857d7e6b4
Ryan Riginding <marc.riginding@gmail.com>
2012-06-04 15:59:43 +0200

    add full text search

--- app/models/search.rb
+++ app/models/search.rb
@@ -8,7 +8,7 @@ private
     vods = Vod.order(:id)
     vods = vods.where(game_id: game_id) if game_id.present?
     vods = vods.where(show_id: show_id) if show_id.present?
-    vods = vods.where("upper(title) like ?", "%#{keywords.upcase}%") if keywords.present?
+    vods = vods.where("vods.title @@ :q", q: keywords.upcase) if keywords.present?
     vods
   end
 end
